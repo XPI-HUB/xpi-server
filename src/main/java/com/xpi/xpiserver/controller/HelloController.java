a94c39f2b65e7cf1f9d032b61b427c27802c2e7c
avinashvijayvargiya <avinash14022002@gmail.com>
2024-04-26 18:52:11 +0530

    added swagger, hello world and health endpoint

---
+++ src/main/java/com/xpi/xpiserver/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.xpi.xpiserver.controller;
+/*
+ * User: Avinash Vijayvargiya
+ * Date: 26/04/24
+ * Time: 6:38 pm
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
