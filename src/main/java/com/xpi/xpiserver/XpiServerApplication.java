0d38540ddbd68987bdb12559e81a8d0be15db261
avinashvijayvargiya <avinash14022002@gmail.com>
2024-04-26 17:32:49 +0530

    initializing project

---
+++ src/main/java/com/xpi/xpiserver/XpiServerApplication.java
@@ -0,0 +1,14 @@
+package com.xpi.xpiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class XpiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpiServerApplication.class, args);
+    }
+
+}
