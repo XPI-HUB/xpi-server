242174e687b92de0dc4694bc86307a0c4d243d10
avinashvijayvargiya <avinash14022002@gmail.com>
2024-05-04 12:24:27 +0530

    removed patch

--- src/main/java/com/xpi/xpiserver/XpiServerApplication.java
+++ src/main/java/com/xpi/xpiserver/XpiServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
 public class XpiServerApplication {
 
     public static void main(final String[] args) {
