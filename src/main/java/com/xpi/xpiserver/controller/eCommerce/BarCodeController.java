43bdfb8ea0bfac5fa8d3e1e6431b649d79c3c8dd
avinashvijayvargiya <avinash14022002@gmail.com>
2024-05-10 19:07:37 +0530

    e commerce api

--- src/main/java/com/xpi/xpiserver/controller/eCommerce/BarCodeController.java
+++ src/main/java/com/xpi/xpiserver/controller/eCommerce/BarCodeController.java
@@ -1,11 +1,15 @@
 package com.xpi.xpiserver.controller.eCommerce;
 
 import com.xpi.xpiserver.service.eCommerce.BarCodeService;
-import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import net.sourceforge.barbecue.BarcodeException;
 import net.sourceforge.barbecue.output.OutputException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class BarCodeController {
 
-    @Autowired
-    BarCodeService barCodeService;
+    private final BarCodeService barCodeService;
 
-    @GetMapping(path = "/barCode", produces = MediaType.IMAGE_PNG_VALUE)
-    public BufferedImage generateBarCode(final @RequestParam String text)
-            throws OutputException, BarcodeException {
-        return barCodeService.generateBarCode(text);
+    public BarCodeController(final BarCodeService barCodeService) {
+        this.barCodeService = barCodeService;
+    }
+
+    @GetMapping(path = "/barcode", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> generateBarCode(final @RequestParam String text)
+        throws IOException, OutputException, BarcodeException {
+        byte[] bytes = barCodeService.generateBarCode(text);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Disposition", "attachment; filename=your_file_name");
+        return ResponseEntity.ok()
+            .headers(headers)
+            .contentLength(bytes.length)
+            .contentType(MediaType.IMAGE_PNG)
+            .body(new InputStreamResource(new ByteArrayInputStream(bytes)));
     }
 }
--- src/main/java/com/xpi/xpiserver/service/eCommerce/BarCodeService.java
+++ src/main/java/com/xpi/xpiserver/service/eCommerce/BarCodeService.java
@@ -1,7 +1,6 @@
 package com.xpi.xpiserver.service.eCommerce;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import net.sourceforge.barbecue.Barcode;
@@ -11,19 +10,15 @@ import net.sourceforge.barbecue.BarcodeImageHandler;
 import net.sourceforge.barbecue.output.OutputException;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class BarCodeService {
-    public BufferedImage generateBarCode(final String text)
-            throws BarcodeException, OutputException {
-        Barcode barcode = BarcodeFactory.createCode128(text.toLowerCase());
-        File outputfile = new File("barcode.jpg");
-        try {
-            ImageIO.write(BarcodeImageHandler.getImage(barcode), "jpg", outputfile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return BarcodeImageHandler.getImage(barcode);
+    public byte[] generateBarCode(final String text)
+        throws BarcodeException, OutputException, IOException {
+        Barcode barcode = BarcodeFactory.createCode128(text);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(BarcodeImageHandler.getImage(barcode),
+            "png", byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
 }
